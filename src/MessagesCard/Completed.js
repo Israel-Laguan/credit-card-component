927323846735c681e3178a7d768028349c3d1c07
Jesus <jesusdavidgonzales1102@gmail.com>
2022-12-19 18:09:59 -0500

    Card data processing view

---
+++ src/MessagesCard/Completed.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../MessagesCard/styles/Style-MessagesCard.css'
+
+function Completed(){
+    return(
+        <div id="completed" className="completed">
+            <p>Successful payment</p>
+            <a href="/">Your shopping</a>
+        </div>
+    )
+}
+
+export default Completed
---
+++ src/MessagesCard/Incompleted.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Incompleted(){
+    return(
+        <div className="incompleted" id="incompleted">
+            <p>Error</p>
+            <p>Try it again later</p>
+            <a href="/">Acept</a>  
+        </div>
+    )
+}
+
+export default Incompleted
